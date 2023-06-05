a4555fdffc2a5ac73a1652639a5fdf27162d8549
AngeloAugustoCSW <angelo.weimar@hotmail.com>
2023-06-05 19:55:28 -0300

    Recuperação de senha arrumado

--- src/screens/RecoveryScreen.jsx
+++ src/screens/RecoveryScreen.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { View, TouchableOpacity, SafeAreaView } from "react-native";
 import { TextInput, Text, Dialog, Button } from "react-native-paper";
 import { styles } from "../utils/styles";
-import { auth } from "../config/firebase";
+import { auth, passwordReset } from "../config/firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import Icon from "react-native-vector-icons/FontAwesome";
 import Logo from "../components/Logo";
@@ -14,26 +14,30 @@ export default function RecoveryScreen({ navigation }) {
   const [mailUser, setMailUser] = useState("");
   const [dialogVisible, setDialogVisible] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const [isEmailValid, setIsEmailValid] = useState("false");
 
   const handleRecovery = async (email) => {
     if (mailUser.trim() === "") {
-        setErrorMessage("Por favor, insira o e-mail.");
-        return;
+      setErrorMessage("Por favor, insira o e-mail.");
+      return;
     }
     try {
-      await auth.sendPasswordResetEmail(email);
+      // await auth.sendPasswordResetEmail(email);
+      passwordReset(email);
       setDialogVisible(true);
       Alert.alert(
         "Sucesso",
         "Um e-mail de recuperação foi enviado para o endereço informado."
       );
     } catch (error) {
-      if (error.code === "auth/user-not-found") {// Email não encontrado
+      if (error.code === "auth/user-not-found") {
+        // Email não encontrado
         Alert.alert(
           "Erro",
           "Este usuário não existe. Por favor, verifique o e-mail."
         );
-      } else if (error.code === "auth/invalid-email") { //Email Invalido
+      } else if (error.code === "auth/invalid-email") {
+        //Email Invalido
         Alert.alert(
           "Erro",
           "Endereço de e-mail inválido. Por favor, verifique e tente novamente."
@@ -51,31 +55,32 @@ export default function RecoveryScreen({ navigation }) {
 
     if (mailUser === "") {
       setErrorMessage("");
+      setIsEmailValid(false);
     } else if (!emailRegex.test(mailUser)) {
       setErrorMessage("E-mail informado é inválido.");
+      setIsEmailValid(false);
     } else {
       checkExistingEmail(mailUser);
+      setIsEmailValid(true);
     }
   };
 
   const checkExistingEmail = async (mailUser) => {
-  try{
-    const snapshot = await 
-    Firebase.firestore()
-    .collection('users')
-    .where('email', '==', mailUser)
-    .get()
-    
-    if (!snapshot.empty) {
-      setErrorMessage("E-mail já cadastrado.", error);
-    } else {
-      setErrorMessage("");
-    }
+    try {
+      const snapshot = await Firebase.firestore()
+        .collection("users")
+        .where("email", "==", mailUser)
+        .get();
 
-  }catch{
-    console.log("Erro ao validar e-mail.")
-    return false;
-  }
+      if (!snapshot.empty) {
+        setErrorMessage("E-mail já cadastrado.", error);
+      } else {
+        setErrorMessage("");
+      }
+    } catch {
+      console.log("Erro ao validar e-mail.");
+      return false;
+    }
   };
 
   return (
@@ -101,8 +106,7 @@ export default function RecoveryScreen({ navigation }) {
             borderRadius={10}
             style={styles.button}
             onPress={() => handleRecovery(mailUser)}
-
-            disabled={!mailUser}
+            disabled={!isEmailValid || mailUser.trim() === ""}
           >
             <Text style={styles.buttonText}>Recuperar Senha</Text>
           </TouchableOpacity>
