37c489cc2dd7b8be7ccff3014121f2dc9db5098a
AnaStadelhofer <ana.stadelhofer@outlook.com>
2023-06-12 19:06:19 -0300

    commit

--- src/screens/MenuScreen.jsx
+++ src/screens/MenuScreen.jsx
@@ -29,8 +29,7 @@ function OptionsMenu({ navigation }) {
       <Logo />
       <SafeAreaView>
         <Text style={styles.textInittial}>
-          {" "}
-          Seja bem-vindo ao nosso aplicativo!{" "}
+          Seja bem-vindo ao Priset!
         </Text>
         <TouchableOpacity
           style={styles.button}
